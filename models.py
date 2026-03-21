814755b177bec2033283ea1b2b23299fdbfc85c8
Niv1306 <niveditawaghmare1306@gmail.com>
2026-03-21 21:24:40 +0530

    updated

--- models.py
+++ models.py
@@ -4,6 +4,13 @@ db = SQLAlchemy()
 
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    domain = db.Column(db.String(255), nullable=False)
+    record_count = db.Column(db.Integer, default=0)
+    pass_rate = db.Column(db.Float, default=0)
 
 class Incident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    severity = db.Column(db.String(50))
+    domain = db.Column(db.String(255))
+    title = db.Column(db.String(255))
+    status = db.Column(db.String(50), default="Open")
